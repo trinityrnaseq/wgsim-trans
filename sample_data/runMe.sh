af3e4ac8e015b723080bce500e2593373c877aad
Brian Haas <brianjohnhaas@gmail.com>
2016-11-29 10:33:05 -0500

    update to sample runner

--- sample_data/runMe.sh
+++ sample_data/runMe.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-../wgsim-trans  -e 0 -N 10 -Z 2 trans.fa left.fq right.fq
+set -veou pipefail
+
+../wgsim-trans  -r 0 -N 1000 -Z 2 trans.fa left.fq right.fq
+
+bowtie2-build trans.fa trans.fa
+
+bowtie2 --local --no-unal -x trans.fa -q  -1 left.fq -2 right.fq | samtools view -Sb - | samtools sort -  alignments
+
+samtools faidx trans.fa
+
+samtools index alignments.bam
+
+igv.sh -g `pwd`/trans.fa `pwd`/alignments.bam
+
+
 
